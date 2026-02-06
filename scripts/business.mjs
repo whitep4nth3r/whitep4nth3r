477b4bbb5b16cc229fd6c2e8bf83b8cf9b15b81e
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2026-02-06 18:06:08 +0000

    update business card

--- scripts/business.mjs
+++ scripts/business.mjs
@@ -17,7 +17,6 @@ const intro = chalk.bold("I write code for your entertainment.\n\nFind me on the
 
 const links = [
   { name: chalk.hex("#fff").bgHex("#0f111a")("Website"), url: "   https://whitep4nth3r.com" },
-  { name: chalk.hex("#fff").bgHex("#000")("DEV.to"), url: "    https://dev.to/whitep4nth3r" },
   { name: chalk.hex("#fff").bgHex("#5865F2")("Discord"), url: "   https://discord.gg/theclaw" },
   {
     name: chalk.hex("#fff").bgHex("#6e5494")("GitHub"),
@@ -27,7 +26,6 @@ const links = [
     name: chalk.hex("#fff").bgHex("##208BFE")("Bluesky"),
     url: "   https://bsky.app/profile/whitep4nth3r.com",
   },
-  { name: chalk.hex("#fff").bgHex("#9146ff")("Twitch"), url: "    https://twitch.tv/whitep4nth3r" },
   {
     name: chalk.hex("#fff").bgHex("#ff0000")("YouTube"),
     url: "   https://youtube.com/whitep4nth3r",
